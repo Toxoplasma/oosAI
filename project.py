7fb57c2fb5415e5dacf3d6a40f2601ea508d0bcb
Ravi <rkumar@g.hmc.edu>
2014-04-02 22:01:45 -0700

    started the memory reader

---
+++ project.py
@@ -0,0 +1,34 @@
+#Test reading in memory
+
+BIN_XPOS = 12
+BIN_YPOS = 10
+
+def readGameStateFromFile:
+	xPos = 0
+	yPos = 0
+
+
+	#Open in read binary mode
+	f = open("gb_wram.bin", "rb")
+
+	byteIndex = 0
+	try:
+	    byte = f.read(1)
+	    while byte != "":
+	        # Do stuff with byte.
+	        byte = f.read(1)
+
+	        if byteIndex = BIN_XPOS:
+	        	xPos = byte
+	        if byteIndex = BIN_YPOS:
+	        	yPos = byte
+
+
+	        byteIndex += 1
+	finally:
+	    f.close()
+
+	return xPos, yPos
+
+
+
