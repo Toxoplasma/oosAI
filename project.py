1f6c196947e811e98ac9521a04975bca916bcb96
slfawaz <sfawaz@g.hmc.edu>
2014-04-02 23:13:19 -0700

    Fixed syntax and added more variables

--- project.py
+++ project.py
@@ -1,38 +1,52 @@
 #Test reading in memory
-import sleep
+import time
 
 BIN_XPOS = 4109
 BIN_YPOS = 4107
+BIN_ORIENT = 4146
 
-def readGameStateFromFile:
-	xPos = 0
-	yPos = 0
+BIN_BOSS_YPOS = 4299
+BIN_BOSS_XPOS = 4301
+
+def readGameStateFromFile():
+    xPos = 0
+    yPos = 0
+    orient = 0
+    bossXPos = 0
+    bossYPos = 0
+
+    #Open in read binary mode
+    f = open("gb_wram.bin", "rb")
+
+    byteIndex = 0
+    try:
+        byte = " "
+        while byte != "":
+            # Do stuff with byte.
+            byte = f.read(1)
+            if byte != "":
+                byte = ord(byte)
+
+            if byteIndex == BIN_XPOS:
+                xPos = byte
+            if byteIndex == BIN_YPOS:
+                yPos = byte
+            if byteIndex == BIN_ORIENT:
+                orient = byte
+            if byteIndex == BIN_BOSS_XPOS:
+                bossXPos = byte
+            if byteIndex == BIN_BOSS_YPOS:
+                bossYPos = byte
 
 
-	#Open in read binary mode
-	f = open("gb_wram.bin", "rb")
+            byteIndex += 1
+    finally:
+        f.close()
 
-	byteIndex = 0
-	try:
-	    byte = f.read(1)
-	    while byte != "":
-	        # Do stuff with byte.
-	        byte = f.read(1)
-
-	        if byteIndex = BIN_XPOS:
-	        	xPos = byte
-	        if byteIndex = BIN_YPOS:
-	        	yPos = byte
-
-
-	        byteIndex += 1
-	finally:
-	    f.close()
-
-	return xPos, yPos
+    return xPos, yPos, orient, bossXPos, bossYPos
 
 
 while True:
-	time.sleep(1)
+    time.sleep(1)
 
-	print readGameStateFromFile()
+    print readGameStateFromFile()
