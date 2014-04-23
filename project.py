d4c029866d63d3b5c2e8939c27f180b14d83bf80
slfawaz <sfawaz@g.hmc.edu>
2014-04-22 20:40:23 -0700

    syntax blurgs

--- project.py
+++ project.py
@@ -317,7 +317,7 @@ class QAgent():
 
         #Do we run into aqua?
         if -35 < xdif and xdif < 25 and \
-           -50 < ydif and ydif < 20 and:
+           -50 < ydif and ydif < 20:
            feat['insideBoss'] = 1.0
 
 
@@ -454,9 +454,9 @@ while True:
         print "Action is: " + action
 
     #Do the action for one STEP
-    #win32api.keybd_event(ACTION_TO_VKEY[action], ACTION_TO_SKEY[action])
-    #time.sleep(STEPSIZE)
-    #win32api.keybd_event(ACTION_TO_VKEY[action], ACTION_TO_SKEY[action], 2)
+    win32api.keybd_event(ACTION_TO_VKEY[action], ACTION_TO_SKEY[action])
+    time.sleep(STEPSIZE)
+    win32api.keybd_event(ACTION_TO_VKEY[action], ACTION_TO_SKEY[action], 2)
 
     #Get the reward and update the weights
     gameIsOver = False
